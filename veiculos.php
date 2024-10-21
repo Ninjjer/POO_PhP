1e429f11202c01266fc378ce9649c07c80302918
Ninjjer <henrique.gianottiv@gmail.com>
2024-10-21 07:32:42 -0400

    Arrumado algumas coisas de acordo com as boas práticas

--- veiculos.php
+++ veiculos.php
@@ -1,18 +1,21 @@
 <?php 
 
-    abstract class veiculo{
+    abstract class veiculo
+    {
         //atributos = são características que nossos objetos terão
         public $nome;
         public $ano;
         public $valor;        
 
-        public function __construct($nome, $ano, $valor){
-            $this->nome = $nome;
-            $this->ano = $ano;
-            $this->valor = $valor;
+        public function __construct($dados)
+        {
+            $this->nome = $dados;
+            $this->ano = $dados;
+            $this->valor = $dados;
         }
 
-        public function getNome(){
+        public function getNome()
+        {
             return $this->nome;
         }
 
@@ -26,121 +29,137 @@
             }*/
     }
 
-    class carro extends veiculo{
+    class carro extends veiculo
+    {
         /*
         public function abrirPorta(){
             echo "Abrir porta";
         }*/
-        public function Acelerar(){
+        public function Acelerar()
+        {
             echo "Vrom!";
         }
 
-        public function Freiar(){
+        public function Freiar()
+        {
             echo "Tum!";
         }
     }
 
-    class moto extends veiculo{
+    class moto extends veiculo
+    {
         /*public function darGrau(){
             echo "Empinar a moto";
         }*/
-        public function Acelerar(){
+        public function Acelerar()
+        {
             echo "Ron! Ron!";
         }
         
-        public function Freiar(){
+        public function Freiar()
+        {
             echo "Tum!";
         }
     }
 
-    class barco extends veiculo{
-        public function Acelerar(){
+    class barco extends veiculo
+    {
+        public function Acelerar()
+        {
             echo "Acelera aí";
         }
-        public function Freiar(){
+        public function Freiar()
+        {
             echo "Só parar de acelerar";
         }
     }
 
-    class bicicleta extends veiculo{
-        public function Acelerar(){
+    class bicicleta extends veiculo
+    {
+        public function Acelerar()
+        {
             echo "Pedala!";
         }
-        public function Freiar(){
+        public function Freiar()
+        {
             echo "Aperte o guidão";
         }
     }
 
-    class skate extends veiculo{
-        public function Acelerar(){
+    class skate extends veiculo
+    {
+        public function Acelerar()
+        {
             echo "Empurre o solo com o pé";
         }
-        public function Freiar(){
+        public function Freiar()
+        {
             echo "Só pular do skate pra freiar";
         }
     }
 
-    class aviao extends veiculo{
-        public function Acelerar(){
+    class aviao extends veiculo
+    {
+        public function Acelerar()
+        {
             echo "É igual mexer com linux para acelerar";
         }
-        public function Freiar(){
+        public function Freiar()
+        {
             echo "Use a aerodinâmica para freiar";
         }
     }
 
-    class elevador extends veiculo{
-        public function Acelerar(){
+    class elevador extends veiculo
+    {
+        public function Acelerar()
+        {
             echo "Aperte o botão para locomover";
         }    
-        public function Freiar(){
+        public function Freiar()
+        {
             echo "Só Deus freia isso aqui";
         }
     }
 
-echo "---Carro---<br><br>";
+    
+
+echo "<pre> ---Carro--- \n";
 $carro = new carro("Civic", 2023, 105000);
 $carro -> Acelerar();
-echo "<br>";
 $carro -> Freiar();
 
-echo "<hr> ---Moto--- <br><br>";
+echo "<pre> ---Moto--- \n";
 $moto = new moto("Pop 100", 2010, 10200);
 $moto -> Acelerar();
-echo "<br>";
 $moto -> Freiar();
 
-echo "<hr> ---Barco--- <br><br>";
+echo "<pre> ---Barco--- \n";
 
 $barco = new barco("Astray", 2020, 600000);
 $barco -> Acelerar();
-echo "<br>";
 $barco -> Freiar();
 
-echo "<hr> ---Bicicleta--- <br><br>";
+echo "<pre> ---Bicicleta--- \n";
 
 $bicicleta = new bicicleta("BMX", 2022, 15000);
 $bicicleta -> Acelerar();
-echo "<br>";
 $bicicleta -> Freiar();
 
-echo "<hr> ---Skate--- <br><br>";
+echo "<pre> ---Skate--- \n";
 
 $skate = new skate("Chorão", 2018, 1500000);
 $skate -> Acelerar();
-echo "<br>";
 $skate -> Freiar();
 
-echo "<hr> ---Avião--- <br><br>";
+echo "<pre> ---Avião--- \n";
 
 $aviao = new aviao("Flying-V", 2020, 2000000);
 $aviao -> Acelerar();
-echo "<br>";
 $aviao -> Freiar();
 
-echo "<hr> Vez do elevador <br><br>";
+echo "<pre> Vez do elevador \n";
 
 $elevador = new elevador("Elevador", 2010, 400000);
 $elevador -> Acelerar();
-echo "<br>";
 $elevador -> Freiar();
